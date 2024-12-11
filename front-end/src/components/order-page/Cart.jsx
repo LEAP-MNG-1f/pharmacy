a4dc2e936eb63ca411c8c670c86d3f68ecd53b5f
Jwz-jwz <jnglobal444@gmail.com>
2024-12-11 21:36:20 +0800

    dfd

--- front-end/src/components/order-page/Cart.jsx
+++ front-end/src/components/order-page/Cart.jsx
@@ -4,7 +4,7 @@ import { useDataContext } from "../context/dataContext";
 
 export const Cart = () => {
   const [spaceImage, setSpaceImage] = useState({});
-  const [imagePreview, setImagePreview] = useState();
+  // const [imagePreview, setImagePreview] = useState();
   // let [count, setCount] = useState(0);
   // function Nemeh() {
   //   count = count + 1;
@@ -101,6 +101,32 @@ export const Cart = () => {
               </div>
             );
           })}
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="max-w-[210px] w-full h-[50px] p-2 flex flex-col justify-center items-center gap-2 border border-dashed border-[#D6D7DC] bg-[rgba(186,188,196,0.12)] rounded-lg">
+            <input
+              type="file"
+              id="uploadFile1"
+              name="uploadFile1"
+              className="hidden"
+              onChange={handleFileChange}
+            />
+            <label
+              htmlFor="uploadFile1"
+              className="px-1 py-2 rounded-lg bg-SecondColor text-black font-inter text-base font-bold cursor-pointer"
+            >
+              Жорын зургийг оруулна уу
+            </label>
+          </div>
+          {imagePreview && (
+            <div className="mt-4">
+              <img
+                src={imagePreview}
+                alt="Prescription Preview"
+                className="w-full h-[200px] object-contain border border-black border-dashed p-2"
+              />
+            </div>
+          )}
         </div>
         <button>Захиалах</button>
       </div>
