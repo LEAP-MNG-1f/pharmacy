a62ff7eba8283693fe88ce0828c983360244ae2b
Бадамгарав Орги <24LP1595@G401-CC0119.local>
2024-12-12 13:01:12 +0800

    Search almost ended

--- front-end/src/components/context/dataContext.js
+++ front-end/src/components/context/dataContext.js
@@ -33,6 +33,7 @@ export const DataProvider = ({ children }) => {
     medicines,
     loading,
     error,
+    setMedicines,
   };
 
   return (
--- front-end/src/components/map/GoogleHome.jsx
+++ front-end/src/components/map/GoogleHome.jsx
@@ -1,96 +1,47 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import Select from "react-select";
 import List from "./List";
 import { GoogleMap } from "./Google";
-import { BACKEND_URL } from "../../../constant/constant";
+import { useDataContext } from "../context/dataContext";
 
 export const GoogleHomePage = () => {
-  const [districtOptions, setDistrictOptions] = useState([]);
-  const [filterCity, setFilterCity] = useState([]);
+  const [filterName, setFilterName] = useState([]);
+  const { medicines, setMedicines } = useDataContext();
 
-  const [medicines, setMedicine] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
-  const fetchBeData = async () => {
-    try {
-      const response = await fetch(`${BACKEND_URL}/api/medicines`);
-      const dataMedicines = await response.json();
-      setMedicine(dataMedicines?.result);
-      console.log(dataMedicines);
-    } catch (error) {
-      console.log(error, "Medicine fetch error");
-    }
+  const filteredMedincine = medicines?.filter((medicine) =>
+    medicine?.name?.toLowerCase().includes(searchValue)
+  );
+
+  console.log(filteredMedincine);
+
+  const handleInputChange = (event) => {
+    setIsOpen(true);
+    setSearchValue(event.target.value);
   };
-
-  const fetchData = async () => {
-    try {
-      const uniqueCityOptions = [];
-      const citySet = new Set();
-      medicines.forEach((medicine) => {
-        if (!citySet.has(medicine.name)) {
-          citySet.add(medicine.name);
-          uniqueCityOptions.push({
-            value: medicine.name,
-            label: medicine.name,
-          });
-        }
-      });
-
-      setDistrictOptions(uniqueCityOptions);
-      setMedicine(medicines);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-  console.log(medicines);
-
-  const handleSelectCityChange = (selectedOptions) => {
-    const selectedCities = selectedOptions.map((option) => option.value);
-
-    const newSelectedLocations = medicines
-      .filter((medicine) => selectedCities.includes(medicine.name))
-      .map((medicine) => ({
-        _id: medicine._id,
-        lat: parseFloat(medicine.lat),
-        lng: parseFloat(medicine.long),
-        categoryId: medicine.categoryId,
-        name: medicine.name,
-        balance: medicine.balance,
-        imageUrl: medicine.image,
-        location: medicine.location,
-        type: medicine.recipeType,
-      }));
-    setFilterCity(newSelectedLocations);
-  };
-
-  useEffect(() => {
-    fetchData();
-    fetchBeData();
-  }, []);
 
   return (
     <main className="w-screen flex justify-center items-center">
-      <div>
-        {medicines.map((medicine) => {
-          return <div key={medicine.id}>{medicine.name}</div>;
-        })}
-      </div>
+      <input
+        placeholder="Search"
+        value={searchValue}
+        type="text"
+        className="bg-[#E8E8EA] outline-none rounded-md text-black"
+        onChange={handleInputChange}
+      />
       <div className="w-full flex flex-col justify-center items-center max-w-[1366px] max-h-[1024px] mt-7 rounded-3xl">
-        <div className="flex items-center gap-6">
-          <Select
-            defaultValue={[]}
-            isMulti
-            name="name"
-            options={districtOptions}
-            className="basic-multi-select w-[626px] text-black"
-            classNamePrefix="select"
-            onChange={handleSelectCityChange}
-          />
-        </div>
+        {filteredMedincine.map((medicine) => {
+          return (
+            <div key={medicine?._id} className="flex flex-col">
+              <List selectedLocation={medicine} />
+            </div>
+          );
+        })}
 
         <div className="flex">
-          <List selectedLocation={filterCity} />
-          <GoogleMap selectedLocation={filterCity} />
+          <GoogleMap selectedLocation={filteredMedincine} />
         </div>
       </div>
     </main>
