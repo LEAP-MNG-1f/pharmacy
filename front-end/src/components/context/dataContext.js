7b550893b31caca80df01997c739bab8cc5e9575
Jwz-jwz <jnglobal444@gmail.com>
2024-12-16 00:11:14 +0800

    dsds
    :wq

--- front-end/src/components/context/dataContext.js
+++ front-end/src/components/context/dataContext.js
@@ -10,7 +10,8 @@ export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [searchValue, setSearchValue] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [apteks, setAptesks] = useState([]);
+  const [apteks, setApteks] = useState([]);
+  const [yags, setYags] = useState([]);
 
   const fetchDatas = async () => {
     setLoading(true); // Start loading
@@ -27,6 +28,8 @@ export const DataProvider = ({ children }) => {
       setLoading(false); // End loading
     }
   };
+
+  //////emiinsanguudiig fetch hiij baina
   const fetchEmiinSans = async () => {
     setLoading(true); // Start loading
     try {
@@ -36,30 +39,51 @@ export const DataProvider = ({ children }) => {
       }
       const datas = await response.json();
 
-      setAptesks(datas?.data || []); // Safely access the result
+      setApteks(datas?.data || []); // Safely access the result
     } catch (error) {
       setError(error.message); // Set error message
     } finally {
       setLoading(false); // End loading
     }
   };
-  console.log(apteks);
+
+  //yags-g fetch hiij baina///////
+  const fetchYags = async () => {
+    setLoading(true); // Start loading
+    try {
+      const response = await fetch(`${BACKEND_URL}/api/yags`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const datas = await response.json();
+      setYags(datas?.data || []); // Safely access the result
+    } catch (error) {
+      setError(error.message); // Set error message
+    } finally {
+      setLoading(false); // End loading
+    }
+  };
+
+  const uniqueNames = [...new Set(yags.map((item) => item.name))]; //unique nernuudiig yags array-s ylgaj bgaa heseg
 
   useEffect(() => {
+    fetchYags();
     fetchEmiinSans();
     fetchDatas();
   }, []); // Empty dependency array to fetch only on mount
 
-  const hailt = ["Humira", "Adderall"];
+  ///hailtaar iim utga orj irne gej uzeed
 
-  const filteredEmiins = apteks?.filter((aptek) => {
-    // Check if all drugs in the query are in the emsId array
-    return hailt.every((drug) =>
-      aptek.emsId.some((item) => item?.name === drug)
-    );
-  });
+  // const hailt = ["Humira", "Adderall"];
 
-  console.log(filteredEmiins);
+  //hailtaar orj irsen emuudiig aguulsan aptekiig olj baigaa heseg
+
+  // const filteredEmiins = apteks?.filter((aptek) => {
+  //   // Check if all drugs in the query are in the emsId array
+  //   return hailt.every((drug) =>
+  //     aptek.emsId.some((item) => item?.name === drug)
+  //   );
+  // });
 
   const filteredMedincine = medicines?.filter((medicine) =>
     medicine?.name?.toLowerCase().includes(searchValue)
@@ -81,6 +105,8 @@ export const DataProvider = ({ children }) => {
     setIsOpen,
     filteredMedincine,
     handleInputChange,
+    uniqueNames,
+    apteks,
   };
 
   return (
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -6,15 +6,23 @@ import CarouselAds from "@/components/home-page/CarousalAds";
 import Collab from "@/components/home-page/Collab";
 import FAQ from "@/components/home-page/FAQ";
 import GoogleHomePage from "@/components/map/GoogleHome";
+
 import MedicineSelector from "@/components/map/Test";
+
+import SelectMedicines from "@/components/map/TestBadmaaEgch";
+import MedicineSelector from "@/components/map/TestBadmaaEgch";
+import Footer from "@/components/ui/Footer";
+
+import Header from "@/components/ui/Header";
 
 export default function HomePage() {
   return (
     <div className="w-full flex flex-col ">
       <Header />
       <div className="w-full flex  gap-[100px] mt-[80px] h-[1200px]">
-        <CarouselAds />
-        <GoogleHomePage />
+        {/* <CarouselAds /> */}
+        <SelectMedicines />
+        {/* <GoogleHomePage /> */}
         {/* <Collab /> */}
       </div>
       <Advice />
