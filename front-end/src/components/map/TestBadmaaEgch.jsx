01fd4d82e45be30c9c0ff7407ca20234a190a765
Jwz-jwz <jnglobal444@gmail.com>
2024-12-16 00:14:27 +0800

    almost end

--- front-end/src/components/map/TestBadmaaEgch.jsx
+++ front-end/src/components/map/TestBadmaaEgch.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import Select from "react-select";
 import { useDataContext } from "../context/dataContext";
+import { GoogleMap } from "./Google";
 
 const MedicineSelector = () => {
   const { uniqueNames, apteks } = useDataContext();
@@ -26,58 +27,61 @@ const MedicineSelector = () => {
   console.log(filteredEmiinsan);
 
   return (
-    <div className="ml-[300px] flex flex-col gap-[100px]">
-      <div>
-        <h1>Select Medicines</h1>
+    <div className="flex gap-[20px]">
+      <div className="ml-[300px] flex flex-col gap-[100px]">
+        <div>
+          <h1>Select Medicines</h1>
 
-        <Select
-          options={medicineOptions}
-          value={selectedMedicines}
-          onChange={handleMedicineChange}
-          isMulti
-          getOptionLabel={(option) => option.label}
-          placeholder="Select medicines"
-        />
+          <Select
+            options={medicineOptions}
+            value={selectedMedicines}
+            onChange={handleMedicineChange}
+            isMulti
+            getOptionLabel={(option) => option.label}
+            placeholder="Select medicines"
+          />
 
-        <h3>Selected Medicines:</h3>
-        {selectedMedicines.length === 0 ? (
-          <p>No medicines selected.</p>
-        ) : (
-          <ul>
-            {selectedMedicines.map((medicine) => (
-              <li key={medicine.value}>{medicine.label}</li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div>
-        {filteredEmiinsan?.map((emiinsan) => {
-          return (
-            <div key={emiinsan._id}>
-              <div className="flex gap-[50px]">
-                <div className="flex flex-col gap-[100px]">
-                  <img src={emiinsan?.image} alt="" />
-                  <p>Hayg:{emiinsan?.location}</p>
-                </div>
-                <div className="flex flex-col gap-[20px]">
-                  {emiinsan?.emsId.map((em) => {
-                    return (
-                      <div
-                        key={em._id}
-                        className="flex flex-col gap-[10px] border border-green-500 rounded-xl"
-                      >
-                        <div>{em?.name}</div>
-                        <div>{em?.price}</div>
-                        <div>{em?.balance}</div>
-                      </div>
-                    );
-                  })}
+          <h3>Selected Medicines:</h3>
+          {selectedMedicines.length === 0 ? (
+            <p>No medicines selected.</p>
+          ) : (
+            <ul>
+              {selectedMedicines.map((medicine) => (
+                <li key={medicine.value}>{medicine.label}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div>
+          {filteredEmiinsan?.map((emiinsan) => {
+            return (
+              <div key={emiinsan._id}>
+                <div className="flex gap-[50px]">
+                  <div className="flex flex-col gap-[100px]">
+                    <img src={emiinsan?.image} alt="" />
+                    <p>Hayg:{emiinsan?.location}</p>
+                  </div>
+                  <div className="flex flex-col gap-[20px]">
+                    {emiinsan?.emsId.map((em) => {
+                      return (
+                        <div
+                          key={em._id}
+                          className="flex flex-col gap-[10px] border border-green-500 rounded-xl"
+                        >
+                          <div>{em?.name}</div>
+                          <div>{em?.price}</div>
+                          <div>{em?.balance}</div>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
+      <GoogleMap selectedLocation={filteredEmiinsan} />
     </div>
   );
 };
