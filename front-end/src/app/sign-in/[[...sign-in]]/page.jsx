74071736db6eb0983fc268ee3fed31431d6f2e51
Okibuyan <boika.dashdeleg@gmail.com>
2024-12-18 10:39:07 +0800

    back to original

--- front-end/src/app/sign-in/[[...sign-in]]/page.jsx
+++ front-end/src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,29 +1,9 @@
-"use client";
-import { SignIn, useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  const { isLoaded, isSignedIn, user } = useUser();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    console.log("Component state:", { isLoaded, isSignedIn, hasUser: !!user });
-
-    if (isLoaded && isSignedIn && user) {
-      console.log("User ID:", user.id);
-      console.log("First Name:", user.firstName);
-      console.log("Email:", user.emailAddresses[0]?.emailAddress);
-      console.log("Full user object:", user);
-    }
-  }, [isLoaded, isSignedIn, user]);
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignIn
-        signUpUrl="/sign-up"
-        afterSignInUrl={window.location.href} // Stay on current page
-      />
+      <SignIn />
     </div>
   );
 }
